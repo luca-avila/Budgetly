87557eb190b6056ed7f445c8f8d31c12135367d6
luca-avila <avilaluca61@gmail.com>
2025-07-08 16:39:03 -0300

    Fix database location creation

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_default_secret_key')
-    DATABASE_URL = 'sqlite:///database.db'
+    DATABASE_URL = 'sqlite:///backend/database.db'
