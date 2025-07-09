8359b4b212e2bed9b0a7711550937a2add99fe8e
luca-avila <avilaluca61@gmail.com>
2025-07-09 14:59:43 -0300

    Add validation to POST method

--- backend/routes/transactions.py
+++ backend/routes/transactions.py
@@ -6,16 +6,35 @@ transactions_bp = Blueprint('transactions', __name__)
 
 @transactions_bp.route('/transactions', methods=['POST'])
 def add_transaction():
+    # Get json
     data = request.json
 
+    # Return error if not all required fields are received
+    required_fields = ['type', 'amount', 'category']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'error': f'Missing field: {field}'}), 400
+
+    # Return error if amount is not a number
+    try:
+        r_amount = float(data['amount'])
+    except (ValueError, TypeError):
+        return jsonify({'error': 'Amount must be a number'}), 400
+
+    # Return error if wrong transaction type is sended
+    if data['type'] not in ['Buy', 'Income']:
+        return jsonify({'error': 'Transaction type should be Buy or Income'}), 400
+
+    # Add to the database
     with engine.begin() as conn:
         insert_statement = transactions.insert().values(
             type = data['type'], 
-            amount = data['amount'],
+            amount = r_amount,
             category = data['category'], 
             description = data.get('description', ''),
             date = date.today()
         )
         conn.execute(insert_statement)
-        
-    return jsonify({'message': 'Transaction added succesfully'}), 201
+
+    # Return success message
+    return jsonify({'message': 'Transaction added successfully'}), 201
