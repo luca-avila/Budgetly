9f6af51fa21dd5b177fa0bc41a34c92a504ad3d4
luca-avila <avilaluca61@gmail.com>
2025-07-10 18:29:27 -0300

    Add cors inside create_app()

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from backend.app.routes.transactions import transactions_bp
 from backend.app.config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     app.register_blueprint(transactions_bp)
 
